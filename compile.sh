f4f242f8beb0e51bf78d9f56af14ad0ae76f9066
Lucas Azevedo <lhs_azevedo@hotmail.com>
2020-08-20 19:07:09 -0300

    Messes with compile file

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ mkdir -p tmp build
 rm -f tmp/* 
 
 echo Compiling
-wla-z80 -Iasm -o tmp/baserom.o asm/baserom.asm
+wla-z80 -i -Iasm -o tmp/baserom.o asm/baserom.asm
 
 echo Creating link file
 # Make link file
@@ -18,12 +18,11 @@ rm -f build/*
 
 echo Linking
 cd tmp
-wlalink -d -r linkfile ../build/outrom.sms
+wlalink -d -S -A -i -b linkfile ../build/outrom.sms
 cd ..
 
 if cmp -l baserom.sms build/outrom.sms; then
     echo "Success!"
-    rm -rf tmp
 else
     echo "Diff :/"
 fi
