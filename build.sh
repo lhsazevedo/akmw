c076d988782428fe46bde8ae8c43120c3dfb1986
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2020-08-20 04:26:14 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,29 @@
-rm -f baserom.o
-../wla-dx/build/binaries/wla-z80 -o baserom.o asm/baserom.asm -I asm
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# ::             WLA DX compiling batch file v3              ::
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-rm -f outrom.sms
-../wla-dx/build/binaries/wlalink -d -r asm/baserom.link outrom.sms
+mkdir -p tmp build
 
+rm -f tmp/* 
+
+echo Compiling
+wla-z80 -Iasm -o tmp/baserom.o asm/baserom.asm
+
+echo Creating link file
+# Make link file
+echo [objects] > tmp/linkfile
+echo baserom.o >> tmp/linkfile
+
+rm -f build/*
+
+echo Linking
+cd tmp
+wlalink -d -r linkfile ../build/outrom.sms
 cd ..
 
-if cmp -l baserom.sms outrom.sms; then
+if cmp -l baserom.sms build/outrom.sms; then
     echo "Success!"
+    rm -rf tmp
 else
     echo "Diff :/"
 fi
