c22b2fccf1b32c990a1a17b867456492def03805
Lucas Henrique Azevedo <lhs_azevedo@hotmail.com>
2020-08-28 01:28:04 -0300

    Format Mt. Ethernal Stage 1 entity descriptors

--- src/data/levels/mtEthernalStage1/screenEntitiesDescriptors.asm
+++ src/data/levels/mtEthernalStage1/screenEntitiesDescriptors.asm
@@ -1,60 +1,132 @@
-; 1st entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B6ED to B6ED (1 bytes)
+; Data at B6ED (1 bytes)
 MtEternalScreen0EntitiesDescriptor:
-.db $00
+    .db $00
 
-; 2nd entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B6EE to B6F2 (5 bytes)
+; Data at B6EE (5 bytes)
 MtEternalScreen1EntitiesDescriptor:
-.db $01 $2D $78 $68 $00
+    .db $01
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $78 $68
+    .db $00
 
-; 3rd entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B6F3 to B6FB (9 bytes)
+; Data at B6F3 (9 bytes)
 MtEternalScreen2EntitiesDescriptor:
-.db $02 $2D $20 $88 $00 $2D $98 $40 $00
+    ; Length
+    .db $02
+    ; Entity 1
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $20 $88
+    .db $00
+    ; Entity 2
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $98 $40
+    .db $00
 
-; 4th entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B6FC to B704 (9 bytes)
+; Data at B6FC (9 bytes)
 MtEternalScreen3EntitiesDescriptor:
-.db $02 $2D $10 $88 $00 $2D $A0 $60 $00
+    ; Length
+    .db $02
+    ; Entity 1
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $10 $88
+    .db $00
+    ; Entity 2
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $A0 $60
+    .db $00
 
-; 5th entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B705 to B70D (9 bytes)
+; Data at B705 (9 bytes)
 MtEternalScreen4EntitiesDescriptor:
-.db $02 $2D $20 $D0 $00 $2D $70 $88 $00
+    .db $02
+    ; Entity 1
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $20 $D0
+    .db $00
+    ; Entity 2
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $70 $88
+    .db $00
 
-; 6th entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B70E to B712 (5 bytes)
+; Data at B70E (5 bytes)
 MtEternalScreen5EntitiesDescriptor:
-.db $01 $2D $40 $50 $00
+    .db $01
+    ; Entity 1
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $40 $50
+    .db $00
 
-; 7th entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B713 to B71B (9 bytes)
+; Data at B713 (9 bytes)
 MtEternalScreen6EntitiesDescriptor:
-.db $02 $2D $20 $D0 $00 $2D $90 $30 $00
+    .db $02
+    ; Entity 1
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $20 $D0
+    .db $00
+    ; Entity 2
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $90 $30
+    .db $00
 
-; 8th entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B71C to B724 (9 bytes)
+; Data at B71C (9 bytes)
 MtEternalScreen7EntitiesDescriptor:
-.db $02 $2D $70 $28 $00 $2D $98 $B0 $00
+    .db $02
+    ; Entity 1
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $70 $28
+    .db $00
+    ; Entity 2
+    .db ENTITY_MOSTERBIRD_LEFT
+    .db $98 $B0
+    .db $00
 
-; 9th entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B725 to B725 (1 bytes)
+; Data at B725 (1 bytes)
 MtEternalScreen8EntitiesDescriptor:
-.db $00
+    .db $00
 
-; 10th entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B726 to B732 (13 bytes)
+; Data at B726 (13 bytes)
 MtEternalScreen9EntitiesDescriptor:
-.db $03 $30 $68 $38 $00 $30 $50 $80 $00 $30 $78 $E0 $00
+    .db $03
+    ; Entity 1
+    .db ENTITY_SMALL_FISH_LEFT
+    .db $68 $38
+    .db $00
+    ; Entity 2
+    .db ENTITY_SMALL_FISH_LEFT
+    .db $50 $80
+    .db $00
+    ; Entity 3
+    .db ENTITY_SMALL_FISH_LEFT
+    .db $78 $E0
+    .db $00
 
-; 11th entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B733 to B73B (9 bytes)
+; Data at B733 (9 bytes)
 MtEternalScreen10EntitiesDescriptor:
-.db $02 $30 $30 $A0 $00 $30 $80 $90 $00
+    .db $02
+    .db ENTITY_SMALL_FISH_LEFT
+    .db $30 $A0
+    .db $00
+    .db ENTITY_SMALL_FISH_LEFT
+    .db $80 $90
+    .db $00
 
-; 12th entry of Pointer Table from B527 (indexed by v_entityIndex)
-; Data from B73C to B74D (18 bytes)
+; Data at B73C (18 bytes)
+; @FIXME Theres something wrong here!
 MtEternalScreen11EntitiesDescriptor:
-.db $84 $44 $30 $E0 $00 $03 $2E $28 $A0 $00 $30 $68 $80 $00 $23 $80
-.db $30 $00
+    .db $84 ; @TODO: Why bit 7 is set?
+    .db $44 ; And why this extra value?
+    ; Entity 1
+    .db ENTITY_SMALL_FISH_LEFT
+    .db $E0 $00
+    .db $03
+    ; Entity 2
+    .db $2E
+    .db $28 $A0
+    .db $00
+    ; Entity 3
+    .db ENTITY_SMALL_FISH_LEFT
+    .db $68 $80
+    .db $00
+    ; Entity 4
+    .db $23
+    .db $80 $30
+    .db $00
