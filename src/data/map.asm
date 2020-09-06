7970052810ed295a8c4f81d6595674852d293ccf
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2020-09-06 15:52:10 -0300

    Extract map to file

---
+++ src/data/map.asm
@@ -0,0 +1,173 @@
+; Pointer Table from 15E45 to 15E74 (24 entries, indexed by _RAM_C03C_)
+_DATA_15E45_:
+.dw _DATA_15E75_ _DATA_15E99_ _DATA_15EBD_ _DATA_15EE1_ _DATA_15F05_ _DATA_15F29_ _DATA_15F4D_ _DATA_15F71_
+.dw _DATA_15F95_ _DATA_15FB9_ _DATA_15FDD_ _DATA_16001_ _DATA_16025_ _DATA_16049_ _DATA_1606D_ _DATA_16091_
+.dw _DATA_160B5_ _DATA_160D9_ _DATA_160FD_ _DATA_16121_ _DATA_16145_ _DATA_16169_ _DATA_1618D_ _DATA_161B1_
+
+; 1st entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15E75 to 15E98 (36 bytes)
+_DATA_15E75_:
+.db $01 $00 $08 $00 $08 $00 $08 $00 $08 $00 $08 $00 $08 $00 $08 $00
+.db $08 $00 $08 $00 $08 $00 $08 $00 $08 $00 $08 $00 $08 $00 $08 $00
+.db $63 $00 $65 $00
+
+; 2nd entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15E99 to 15EBC (36 bytes)
+_DATA_15E99_:
+.db $02 $00 $09 $00 $09 $00 $09 $00 $09 $00 $09 $00 $09 $00 $09 $00
+.db $09 $00 $09 $00 $09 $00 $09 $00 $09 $00 $09 $00 $09 $00 $09 $00
+.db $64 $00 $66 $00
+
+; 3rd entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15EBD to 15EE0 (36 bytes)
+_DATA_15EBD_:
+.db $03 $00 $0A $00 $12 $00 $1C $00 $24 $00 $0A $00 $0A $00 $20 $00
+.db $10 $02 $18 $02 $0E $00 $16 $00 $0A $00 $0A $00 $0A $00 $61 $00
+.db $03 $04 $00 $00
+
+; 4th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15EE1 to 15F04 (36 bytes)
+_DATA_15EE1_:
+.db $04 $00 $0B $00 $13 $00 $1D $00 $25 $00 $0A $00 $20 $00 $20 $00
+.db $0F $02 $17 $02 $0F $00 $17 $00 $11 $00 $0A $00 $10 $02 $62 $00
+.db $04 $04 $00 $00
+
+; 5th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15F05 to 15F28 (36 bytes)
+_DATA_15F05_:
+.db $05 $00 $0C $00 $14 $00 $1E $00 $26 $00 $2B $00 $30 $00 $36 $00
+.db $0E $02 $16 $02 $10 $00 $18 $00 $50 $00 $56 $00 $5D $00 $17 $02
+.db $03 $04 $00 $00
+
+; 6th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15F29 to 15F4C (36 bytes)
+_DATA_15F29_:
+.db $06 $00 $0D $00 $15 $00 $1F $00 $11 $00 $2C $00 $31 $00 $37 $00
+.db $3C $00 $41 $00 $46 $00 $4B $00 $51 $00 $57 $00 $0E $02 $16 $02
+.db $04 $04 $00 $00
+
+; 7th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15F4D to 15F70 (36 bytes)
+_DATA_15F4D_:
+.db $07 $00 $0E $00 $16 $00 $20 $00 $11 $00 $2D $00 $32 $00 $38 $00
+.db $3D $00 $42 $00 $47 $00 $4C $00 $52 $00 $58 $00 $10 $02 $18 $02
+.db $07 $00 $00 $00
+
+; 8th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15F71 to 15F94 (36 bytes)
+_DATA_15F71_:
+.db $07 $00 $0F $00 $17 $00 $11 $00 $20 $00 $2E $00 $33 $00 $39 $00
+.db $3E $00 $43 $00 $48 $00 $4D $00 $20 $00 $59 $00 $0F $02 $17 $02
+.db $07 $00 $00 $00
+
+; 9th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15F95 to 15FB8 (36 bytes)
+_DATA_15F95_:
+.db $04 $02 $10 $00 $18 $00 $21 $00 $27 $00 $0A $00 $34 $00 $3A $00
+.db $3F $00 $44 $00 $49 $00 $4E $00 $1B $00 $1B $00 $0E $02 $16 $02
+.db $07 $00 $00 $00
+
+; 10th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15FB9 to 15FDC (36 bytes)
+_DATA_15FB9_:
+.db $03 $02 $0A $00 $19 $00 $22 $00 $28 $00 $2F $00 $35 $00 $3B $00
+.db $40 $00 $45 $00 $4A $00 $4F $00 $53 $00 $5A $00 $5E $00 $11 $00
+.db $03 $04 $00 $00
+
+; 11th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 15FDD to 16000 (36 bytes)
+_DATA_15FDD_:
+.db $07 $00 $0A $00 $1A $00 $23 $00 $29 $00 $2F $02 $35 $00 $35 $00
+.db $2F $00 $20 $00 $0F $02 $17 $02 $54 $00 $5B $00 $5F $00 $1B $00
+.db $04 $04 $00 $00
+
+; 12th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 16001 to 16024 (36 bytes)
+_DATA_16001_:
+.db $07 $00 $11 $00 $1B $00 $1B $00 $2A $00 $10 $02 $18 $02 $0A $00
+.db $2F $02 $20 $00 $0E $02 $16 $02 $55 $00 $5C $00 $60 $00 $1B $00
+.db $04 $06 $00 $00
+
+; 13th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 16025 to 16048 (36 bytes)
+_DATA_16025_:
+.db $03 $00 $1B $00 $1B $00 $1B $00 $1B $00 $0F $02 $17 $02 $10 $02
+.db $18 $02 $0A $00 $12 $00 $1C $00 $24 $00 $10 $02 $18 $02 $1B $00
+.db $03 $06 $00 $00
+
+; 14th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 16049 to 1606C (36 bytes)
+_DATA_16049_:
+.db $04 $00 $68 $00 $6D $00 $73 $00 $1B $00 $0E $02 $16 $02 $0F $02
+.db $17 $02 $0B $00 $13 $00 $1D $00 $25 $00 $0F $02 $17 $02 $1B $00
+.db $07 $00 $00 $00
+
+; 15th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 1606D to 16090 (36 bytes)
+_DATA_1606D_:
+.db $07 $00 $69 $00 $6E $00 $74 $00 $7B $00 $7E $00 $20 $00 $0E $02
+.db $16 $02 $84 $00 $87 $00 $1E $00 $26 $00 $0E $02 $16 $02 $97 $00
+.db $9B $00 $00 $00
+
+; 16th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 16091 to 160B4 (36 bytes)
+_DATA_16091_:
+.db $67 $00 $6A $00 $6F $00 $75 $00 $7C $00 $7F $00 $80 $00 $81 $00
+.db $0A $00 $0A $00 $15 $00 $1F $00 $0A $00 $0A $00 $10 $02 $18 $02
+.db $07 $00 $00 $00
+
+; 17th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 160B5 to 160D8 (36 bytes)
+_DATA_160B5_:
+.db $03 $00 $6B $00 $70 $00 $76 $00 $11 $00 $10 $02 $18 $02 $82 $00
+.db $83 $00 $85 $00 $0A $00 $1B $00 $1B $00 $0A $00 $0F $02 $17 $02
+.db $03 $04 $00 $00
+
+; 18th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 160D9 to 160FC (36 bytes)
+_DATA_160D9_:
+.db $04 $00 $6C $00 $71 $00 $77 $00 $11 $00 $0F $02 $17 $02 $11 $00
+.db $0A $00 $86 $00 $88 $00 $8B $00 $8F $00 $93 $00 $96 $00 $98 $00
+.db $04 $04 $00 $00
+
+; 19th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 160FD to 16120 (36 bytes)
+_DATA_160FD_:
+.db $03 $00 $6C $02 $71 $02 $78 $00 $1B $00 $0E $02 $16 $02 $0A $00
+.db $0A $00 $0A $00 $89 $00 $8C $00 $90 $00 $94 $00 $18 $02 $99 $00
+.db $03 $04 $00 $00
+
+; 20th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 16121 to 16144 (36 bytes)
+_DATA_16121_:
+.db $04 $00 $11 $00 $70 $02 $76 $02 $79 $00 $1B $00 $10 $02 $18 $02
+.db $1B $00 $1B $00 $8A $00 $8D $00 $91 $00 $95 $00 $17 $02 $9A $00
+.db $04 $04 $00 $00
+
+; 21st entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 16145 to 16168 (36 bytes)
+_DATA_16145_:
+.db $07 $00 $0A $00 $72 $00 $79 $00 $7D $00 $72 $00 $0F $02 $17 $02
+.db $0A $00 $1B $00 $0A $00 $8E $00 $92 $00 $0E $02 $16 $02 $1B $00
+.db $07 $00 $00 $00
+
+; 22nd entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 16169 to 1618C (36 bytes)
+_DATA_16169_:
+.db $07 $00 $0A $00 $0A $00 $7A $00 $7A $00 $1B $00 $0E $02 $16 $02
+.db $1B $00 $0A $00 $0A $00 $0A $00 $0A $00 $0A $00 $11 $00 $1B $00
+.db $07 $00 $00 $00
+
+; 23rd entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 1618D to 161B0 (36 bytes)
+_DATA_1618D_:
+.db $02 $02 $09 $02 $09 $02 $09 $02 $09 $02 $09 $02 $09 $02 $09 $02
+.db $09 $02 $09 $02 $09 $02 $09 $02 $09 $02 $09 $02 $09 $02 $09 $02
+.db $64 $02 $66 $02
+
+; 24th entry of Pointer Table from 15E45 (indexed by _RAM_C03C_)
+; Data from 161B1 to 161D4 (36 bytes)
+_DATA_161B1_:
+.db $01 $02 $08 $02 $08 $02 $08 $02 $08 $02 $08 $02 $08 $02 $08 $02
+.db $08 $02 $08 $02 $08 $02 $08 $02 $08 $02 $08 $02 $08 $02 $08 $02
+.db $63 $02 $65 $02
