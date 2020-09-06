970bb1882293e582910d6fb78dfd4c119243e6fd
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2020-09-06 10:47:32 -0300

    Extract palettes to file

---
+++ src/data/palettes.asm
@@ -0,0 +1,95 @@
+; 1st entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FC9E to 1FCBD (32 bytes)
+mtEthernalStage1Palette:
+.db $30 $3F $37 $3B $0F $0B $00 $2F $06 $0B $01 $3E $38 $0C $08 $3C
+.db $30 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 15th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FCBE to 1FCDD (32 bytes)
+_DATA_1FCBE_:
+.db $30 $3F $37 $3B $0F $0B $00 $06 $25 $2A $01 $2F $38 $0C $08 $3C
+.db $30 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 3rd entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FCDE to 1FCFD (32 bytes)
+_DATA_1FCDE_:
+.db $30 $3F $37 $3B $0F $0B $00 $2F $06 $0B $01 $3E $38 $0C $08 $3C
+.db $38 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 4th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FCFE to 1FD1D (32 bytes)
+_DATA_1FCFE_:
+.db $30 $3F $06 $01 $0F $0B $00 $2A $25 $3C $02 $03 $38 $0C $08 $00
+.db $30 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 5th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FD1E to 1FD3D (32 bytes)
+_DATA_1FD1E_:
+.db $30 $3F $03 $03 $0F $0B $00 $2F $06 $0B $01 $3E $38 $0C $08 $3C
+.db $38 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 6th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FD3E to 1FD5D (32 bytes)
+_DATA_1FD3E_:
+.db $30 $3F $37 $3B $0F $0B $00 $06 $25 $2A $01 $2F $38 $0C $08 $3C
+.db $30 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 7th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FD5E to 1FD7D (32 bytes)
+_DATA_1FD5E_:
+.db $01 $3F $37 $3B $0F $0B $00 $2F $06 $0B $30 $3E $38 $03 $02 $01
+.db $01 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 8th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FD7E to 1FD9D (32 bytes)
+_DATA_1FD7E_:
+.db $30 $3F $37 $3B $0F $0B $00 $06 $25 $04 $01 $2F $38 $0C $08 $04
+.db $04 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 11th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FD9E to 1FDBD (32 bytes)
+_DATA_1FD9E_:
+.db $25 $3F $37 $3B $0F $0B $00 $2F $30 $3E $20 $34 $38 $06 $01 $05
+.db $25 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 9th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FDBE to 1FDDD (32 bytes)
+_DATA_1FDBE_:
+.db $30 $3F $37 $03 $0F $0B $00 $2F $06 $0B $01 $3E $38 $0C $08 $3C
+.db $30 $3F $05 $0B $03 $02 $00 $30 $06 $0C $0F $08 $3A $36 $03 $0A
+
+; 10th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FDDE to 1FDFD (32 bytes)
+_DATA_1FDDE_:
+.db $30 $3F $37 $3B $0F $0B $00 $06 $25 $2A $01 $2F $38 $0C $08 $3C
+.db $30 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 12th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FDFE to 1FE1D (32 bytes)
+_DATA_1FDFE_:
+.db $30 $3F $37 $3B $0F $0B $00 $06 $25 $2A $01 $2F $38 $0C $08 $3C
+.db $30 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 2nd entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FE1E to 1FE3D (32 bytes)
+_DATA_1FE1E_:
+.db $30 $3F $03 $03 $0F $0B $00 $06 $25 $2A $01 $2F $38 $0C $08 $3C
+.db $30 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 14th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FE3E to 1FE5D (32 bytes)
+_DATA_1FE3E_:
+.db $25 $3F $37 $3B $0F $0B $00 $2F $30 $0F $02 $07 $0B $06 $01 $3C
+.db $25 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 16th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FE5E to 1FE7D (32 bytes)
+_DATA_1FE5E_:
+.db $20 $3F $37 $3B $0F $0B $00 $2F $30 $3E $3C $3E $38 $06 $01 $05
+.db $20 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
+
+; 17th entry of Pointer Table from 1112 (indexed by v_level)
+; Data from 1FE7E to 1FFFF (386 bytes)
+_DATA_1FE7E_:
+.db $30 $3F $37 $3B $0F $0B $00 $2F $06 $0B $01 $3E $38 $0C $08 $3C
+.db $30 $3F $05 $0B $03 $02 $00 $30 $3C $0C $0F $08 $3A $36 $03 $0A
