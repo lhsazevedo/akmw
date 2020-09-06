bd8a1575a300cee35452722c921c467a0a27df79
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2020-09-06 15:54:37 -0300

    Extract shop data to file

---
+++ src/data/shop.asm
@@ -0,0 +1,176 @@
+; 1st entry of Pointer Table from 1F89 (indexed by v_level)
+; Data from 1BE45 to 1BE46 (2 bytes)
+_DATA_1BE45_:
+.db $94 $CA
+
+; Pointer Table from 1BE47 to 1BE4A (2 entries, indexed by unknown)
+.dw _DATA_1BEF9_ _RAM_CA9C_
+
+; Pointer Table from 1BE4B to 1BE50 (3 entries, indexed by unknown)
+.dw _DATA_1BED5_ _RAM_CAA4_ _DATA_1BE9F_
+
+; 3rd entry of Pointer Table from 1F89 (indexed by v_level)
+; Data from 1BE51 to 1BE52 (2 bytes)
+_DATA_1BE51_:
+.db $94 $CA
+
+; Pointer Table from 1BE53 to 1BE56 (2 entries, indexed by unknown)
+.dw _DATA_1BF1D_ _RAM_CA9C_
+
+; Pointer Table from 1BE57 to 1BE5C (3 entries, indexed by unknown)
+.dw _DATA_1BEB1_ _RAM_CAA4_ _DATA_1BF0B_
+
+; 7th entry of Pointer Table from 1F89 (indexed by v_level)
+; Data from 1BE5D to 1BE5E (2 bytes)
+_DATA_1BE5D_:
+.db $94 $CA
+
+; Pointer Table from 1BE5F to 1BE62 (2 entries, indexed by unknown)
+.dw _DATA_1BEF9_ _RAM_CA9C_
+
+; Pointer Table from 1BE63 to 1BE68 (3 entries, indexed by unknown)
+.dw _DATA_1BEB1_ _RAM_CAA4_ _DATA_1BE8D_
+
+; 9th entry of Pointer Table from 1F89 (indexed by v_level)
+; Data from 1BE69 to 1BE6A (2 bytes)
+_DATA_1BE69_:
+.db $94 $CA
+
+; Pointer Table from 1BE6B to 1BE6E (2 entries, indexed by unknown)
+.dw _DATA_1BF1D_ _RAM_CA9C_
+
+; Pointer Table from 1BE6F to 1BE74 (3 entries, indexed by unknown)
+.dw _DATA_1BEC3_ _RAM_CAA4_ _DATA_1BE9F_
+
+; 11th entry of Pointer Table from 1F89 (indexed by v_level)
+; Data from 1BE75 to 1BE76 (2 bytes)
+_DATA_1BE75_:
+.db $94 $CA
+
+; Pointer Table from 1BE77 to 1BE7A (2 entries, indexed by unknown)
+.dw _DATA_1BEB1_ _RAM_CA9C_
+
+; Pointer Table from 1BE7B to 1BE80 (3 entries, indexed by unknown)
+.dw _DATA_1BEC3_ _RAM_CAA4_ _DATA_1BF0B_
+
+; 15th entry of Pointer Table from 1F89 (indexed by v_level)
+; Data from 1BE81 to 1BE82 (2 bytes)
+_DATA_1BE81_:
+.db $94 $CA
+
+; Pointer Table from 1BE83 to 1BE86 (2 entries, indexed by unknown)
+.dw _DATA_1BEF9_ _RAM_CA9C_
+
+; Pointer Table from 1BE87 to 1BE8C (3 entries, indexed by unknown)
+.dw _DATA_1BED5_ _RAM_CAA4_ _DATA_1BE8D_
+
+; 3rd entry of Pointer Table from 1BE63 (indexed by unknown)
+; Data from 1BE8D to 1BE9E (18 bytes)
+_DATA_1BE8D_:
+.db $C1 $00 $C2 $00 $C0 $00 $00 $00 $90 $48 $00 $00 $00 $00 $91 $48
+.db $00 $00
+
+; 3rd entry of Pointer Table from 1BE4B (indexed by unknown)
+; Data from 1BE9F to 1BEB0 (18 bytes)
+_DATA_1BE9F_:
+.db $C1 $00 $C0 $00 $C0 $00 $00 $00 $92 $48 $92 $4A $00 $00 $93 $48
+.db $94 $48
+
+; 1st entry of Pointer Table from 1BE77 (indexed by unknown)
+; Data from 1BEB1 to 1BEC2 (18 bytes)
+_DATA_1BEB1_:
+.db $C1 $00 $C0 $00 $C0 $00 $00 $00 $95 $48 $96 $48 $00 $00 $97 $48
+.db $98 $48
+
+; 1st entry of Pointer Table from 1BE6F (indexed by unknown)
+; Data from 1BEC3 to 1BED4 (18 bytes)
+_DATA_1BEC3_:
+.db $C1 $00 $C2 $00 $C0 $00 $00 $00 $99 $48 $9A $48 $00 $00 $9B $48
+.db $9C $48
+
+; 1st entry of Pointer Table from 1BE4B (indexed by unknown)
+; Data from 1BED5 to 1BEF8 (36 bytes)
+_DATA_1BED5_:
+.db $C1 $00 $C0 $00 $C0 $00 $00 $00 $A1 $48 $A2 $48 $00 $00 $A3 $48
+.db $A3 $4A $C1 $00 $C0 $00 $C0 $00 $00 $00 $9D $48 $9E $48 $00 $00
+.db $9F $48 $A0 $48
+
+; 1st entry of Pointer Table from 1BE47 (indexed by unknown)
+; Data from 1BEF9 to 1BF0A (18 bytes)
+_DATA_1BEF9_:
+.db $C2 $00 $C0 $00 $C0 $00 $00 $00 $A4 $48 $A5 $48 $00 $00 $A6 $48
+.db $A7 $48
+
+; 3rd entry of Pointer Table from 1BE57 (indexed by unknown)
+; Data from 1BF0B to 1BF1C (18 bytes)
+_DATA_1BF0B_:
+.db $C5 $00 $C0 $00 $C0 $00 $00 $00 $A8 $48 $A9 $48 $00 $00 $AA $48
+.db $AB $48
+
+; 1st entry of Pointer Table from 1BE53 (indexed by unknown)
+; Data from 1BF1D to 1BF50 (52 bytes)
+_DATA_1BF1D_:
+.db $C2 $00 $C0 $00 $C0 $00 $00 $00 $AC $48 $AD $48 $00 $00 $AE $48
+.db $AF $48 $51 $BF $51 $BF $60 $BF $60 $BF $60 $BF $60 $BF $6F $BF
+.db $6F $BF $7E $BF $7E $BF $8D $BF $8D $BF $8D $BF $8D $BF $9C $BF
+.db $9C $BF $9C $BF
+
+; 1st entry of Pointer Table from 1FAB (indexed by v_level)
+; Data from 1BF51 to 1BF51 (1 bytes)
+_DATA_1BF51_:
+.db $07
+
+; Pointer Table from 1BF52 to 1BF59 (4 entries, indexed by unknown)
+.dw _RAM_D7D0_ _RAM_CA94_ _RAM_D506_ _DATA_19CD7_
+
+; Data from 1BF5A to 1BF5F (6 bytes)
+.db $CA $02 $DA $D7 $A4 $CA
+
+; 3rd entry of Pointer Table from 1FAB (indexed by v_level)
+; Data from 1BF60 to 1BF60 (1 bytes)
+_DATA_1BF60_:
+.db $09
+
+; Pointer Table from 1BF61 to 1BF6E (7 entries, indexed by unknown)
+.dw _RAM_D7D0_ _RAM_CA94_ _RAM_D503_ _DATA_19CD7_ _DATA_8CA_ _RAM_D7DA_ _RAM_CAA4_
+
+; 7th entry of Pointer Table from 1FAB (indexed by v_level)
+; Data from 1BF6F to 1BF6F (1 bytes)
+_DATA_1BF6F_:
+.db $07
+
+; Pointer Table from 1BF70 to 1BF77 (4 entries, indexed by unknown)
+.dw _RAM_D7D0_ _RAM_CA94_ _RAM_D503_ _DATA_19CD7_
+
+; Data from 1BF78 to 1BF7D (6 bytes)
+.db $CA $01 $DA $D7 $A4 $CA
+
+; 9th entry of Pointer Table from 1FAB (indexed by v_level)
+; Data from 1BF7E to 1BF7E (1 bytes)
+_DATA_1BF7E_:
+.db $09
+
+; Pointer Table from 1BF7F to 1BF86 (4 entries, indexed by unknown)
+.dw _RAM_D7D0_ _RAM_CA94_ _RAM_D504_ _DATA_19CD7_
+
+; Data from 1BF87 to 1BF8C (6 bytes)
+.db $CA $02 $DA $D7 $A4 $CA
+
+; 11th entry of Pointer Table from 1FAB (indexed by v_level)
+; Data from 1BF8D to 1BF8D (1 bytes)
+_DATA_1BF8D_:
+.db $03
+
+; Pointer Table from 1BF8E to 1BF9B (7 entries, indexed by unknown)
+.dw _RAM_D7D0_ _RAM_CA94_ _RAM_D504_ _DATA_19CD7_ _DATA_8CA_ _RAM_D7DA_ _RAM_CAA4_
+
+; 15th entry of Pointer Table from 1FAB (indexed by v_level)
+; Data from 1BF9C to 1BF9C (1 bytes)
+_DATA_1BF9C_:
+.db $07
+
+; Pointer Table from 1BF9D to 1BFA4 (4 entries, indexed by unknown)
+.dw _RAM_D7D0_ _RAM_CA94_ _RAM_D506_ _DATA_19CD7_
+
+; Data from 1BFA5 to 1BFAA (91 bytes)
+.db $CA $01 $DA $D7 $A4 $CA
