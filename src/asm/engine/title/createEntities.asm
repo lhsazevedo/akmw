5e2a4acda66d253f6269fc29f57c6f2f521ef92c
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2020-09-13 02:59:55 -0300

    Use structs on title entity creation code

--- src/asm/engine/title/createEntities.asm
+++ src/asm/engine/title/createEntities.asm
@@ -1,41 +1,41 @@
 createTitleEntityAlexFlyingPeticopter:
-	ld ix, v_entity1
-	ld (ix+0), $18
+	ld ix, v_entities.1
+	ld (ix + Entity.type), $18
 	ld hl, _RAM_C800_
 	ld (v_entities.1.spriteDescriptorPointer), hl
-	ld (ix+12), $DC
-	ld (ix+14), $46
+	ld (ix + Entity.xPos.high), $DC
+	ld (ix + Entity.yPos.high), $46
 	ret
 
 createTitleEntityAlexRidingBoat:
 	ld ix, v_entities.2
-	ld (ix+0), $18
+	ld (ix + Entity.type), $18
 	ld hl, $C828
 	ld (v_entities.2.spriteDescriptorPointer), hl
-	ld (ix+12), $70
-	ld (ix+14), $7C
+	ld (ix + Entity.xPos.high), $70
+	ld (ix + Entity.yPos.high), $7C
 	ret
 
 createTitleEntitiesJankenFlight:
 	ld ix, _RAM_C340_
-	ld (ix+0), $18
+	ld (ix + Entity.type), $18
 	ld hl, $C850
 	ld (_RAM_C347_), hl
-	ld (ix+12), $18
-	ld (ix+14), $4F
+	ld (ix + Entity.xPos.high), $18
+	ld (ix + Entity.yPos.high), $4F
 	ld ix, _RAM_C3C0_
-	ld (ix+0), $18
+	ld (ix + Entity.type), $18
 	ld hl, $961A
 	ld (_RAM_C3C7_), hl
-	ld (ix+12), $30
-	ld (ix+14), $77
+	ld (ix + Entity.xPos.high), $30
+	ld (ix + Entity.yPos.high), $77
 	ret
 
 createTitleEntityAlexSwiming:
 	ld ix, _RAM_C360_
-	ld (ix+0), $18
+	ld (ix + Entity.type), $18
 	ld hl, $C878
 	ld (_RAM_C367_), hl
-	ld (ix+12), $C9
-	ld (ix+14), $0C
+	ld (ix + Entity.xPos.high), $C9
+	ld (ix + Entity.yPos.high), $0C
 	ret
