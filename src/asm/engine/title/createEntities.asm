b9cbc1f6b98117945756328d2ef99a618e2d82af
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2020-09-16 01:49:58 -0300

    Add ENTITY_STATIC entity type

--- src/asm/engine/title/createEntities.asm
+++ src/asm/engine/title/createEntities.asm
@@ -1,6 +1,6 @@
 createTitleEntityAlexFlyingPeticopter:
 	ld ix, v_entities.1
-	ld (ix + Entity.type), $18
+	ld (ix + Entity.type), ENTITY_STATIC
 	ld hl, _RAM_C800_
 	ld (v_entities.1.spriteDescriptorPointer), hl
 	ld (ix + Entity.xPos.high), $DC
@@ -9,7 +9,7 @@ createTitleEntityAlexFlyingPeticopter:
 
 createTitleEntityAlexRidingBoat:
 	ld ix, v_entities.2
-	ld (ix + Entity.type), $18
+	ld (ix + Entity.type), ENTITY_STATIC
 	ld hl, $C828
 	ld (v_entities.2.spriteDescriptorPointer), hl
 	ld (ix + Entity.xPos.high), $70
@@ -17,14 +17,15 @@ createTitleEntityAlexRidingBoat:
 	ret
 
 createTitleEntitiesJankenFlight:
-	ld ix, _RAM_C340_
-	ld (ix + Entity.type), $18
+	ld ix, v_entities.3
+	ld (ix + Entity.type), ENTITY_STATIC
 	ld hl, $C850
 	ld (_RAM_C347_), hl
 	ld (ix + Entity.xPos.high), $18
 	ld (ix + Entity.yPos.high), $4F
-	ld ix, _RAM_C3C0_
-	ld (ix + Entity.type), $18
+
+	ld ix, v_entities.7
+	ld (ix + Entity.type), ENTITY_STATIC
 	ld hl, $961A
 	ld (_RAM_C3C7_), hl
 	ld (ix + Entity.xPos.high), $30
@@ -32,8 +33,8 @@ createTitleEntitiesJankenFlight:
 	ret
 
 createTitleEntityAlexSwiming:
-	ld ix, _RAM_C360_
-	ld (ix + Entity.type), $18
+	ld ix, v_entities.4
+	ld (ix + Entity.type), ENTITY_STATIC
 	ld hl, $C878
 	ld (_RAM_C367_), hl
 	ld (ix + Entity.xPos.high), $C9
