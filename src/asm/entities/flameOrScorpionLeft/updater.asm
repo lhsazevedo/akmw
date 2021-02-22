9178e61c88be735a3f3abe5285ce6aa1d95d3e23
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2021-02-22 03:47:54 -0300

    Add some comments to flame/scorpion left updater

--- src/asm/entities/flameOrScorpionLeft/updater.asm
+++ src/asm/entities/flameOrScorpionLeft/updater.asm
@@ -2,30 +2,34 @@ updateflameOrScorpionLeft:
     ; if not initialized
     bit 0, (ix + Entity.flags)
     jr nz, +
-        ; Initialize
-        ld a, (ix + Entity.isOffScreenFlags.low)
-        or (ix + Entity.isOffScreenFlags.high)
-        jr nz, _LABEL_5E60_
-        set 0, (ix + Entity.flags)
-        set 1, (ix + Entity.flags)
-        ld (ix + Entity.animationTimer), $10
-        ld (ix + Entity.animationTimerResetValue), $10
-        ld (ix + Entity.xSpeed.low), $80
-        ld (ix + Entity.xSpeed.high), $FF
-        jr _LABEL_5E60_
-
+    ; Initialize
+    ld a, (ix + Entity.isOffScreenFlags.low)
+    or (ix + Entity.isOffScreenFlags.high)
+    jr nz, _LABEL_5E60_
+    set 0, (ix + Entity.flags)
+    set 1, (ix + Entity.flags)
+    ld (ix + Entity.animationTimer), $10
+    ld (ix + Entity.animationTimerResetValue), $10
+    ld (ix + Entity.xSpeed.low), $80
+    ld (ix + Entity.xSpeed.high), $FF
+    jr _LABEL_5E60_
+    
 +:
     call tryToKillAlexIfColliding
+
+    ; if is Scorpion (data == 0)
     ld a, (ix + Entity.data)
     or a
     jr nz, +
     call _LABEL_7D0B_
     jp nc, _LABEL_55A5_
+    ; else
 +:
     ld de, $0900
     call getTileNearEntityWithXYOffset
     rlca
     jr nc, +
+    
 -:
     ld (ix + Entity.type), ENTITY_FLAME_OR_SCORPION_RIGHT
     ld (ix + Entity.xSpeed.low), $80
