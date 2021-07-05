9be8899e8f5ae608f787bf00fcf28cb06cd41124
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2021-07-04 22:27:12 -0300

    Convert messages to plain text

--- src/data/messages.asm
+++ src/data/messages.asm
@@ -1,302 +1,59 @@
-; 1st entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F109 to 1F154 (76 bytes)
 _DATA_1F109_:
-.db $9C $20 $B0 ; Top border
-.db $84 $80 $B0 ; Right border
-.db $9A $60 $B0 ; Bottom border
-.db $83 $C0 $B0 ; Left border
-;            W   E   L   C   O   M   E   !       P   L   E   A   S   E       B   U   Y
-.db $19 $20 $E7 $D5 $DC $D3 $DF $DD $D5 $B1 $B0 $E0 $DC $D5 $D1 $E3 $D5 $B0 $D2 $E5 $E9 $B0 $B0 $B0 $B0 $B0 $B0 
-.db $01 $80 $B0
-.db $98 $60 $B0
-.db $01 $80 $E4 ; "T"
-;            H   E       T   H   I   N   G   S       T   H   A   T       Y   O   U       L   I   K   E   .
-.db $18 $20 $D8 $D5 $B0 $E4 $D8 $D9 $DE $D7 $E3 $B0 $E4 $D8 $D1 $E4 $B0 $E9 $DF $E5 $B0 $DC $D9 $DB $D5 $BE
-.db $00 $00
+.INCBIN "src/data/txt/bin/message1.bin"
 
-; 2nd entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F155 to 1F19C (72 bytes)
 _DATA_1F155_:
-.db $9A $20 $B0
-.db $84 $80 $B0
-.db $98 $60 $B0
-.db $83 $C0 $B0
-;            Y   O   U       A   R   E       S   H   O   R   T       O   N       M   O   N   E   Y   ,
-.db $17 $20 $E9 $DF $E5 $B0 $D1 $E2 $D5 $B0 $E3 $D8 $DF $E2 $E4 $B0 $DF $D6 $B0 $DD $DF $DE $D5 $E9 $BC
-.db $01 $80 $B0
-.db $96 $60 $B0
-.db $01 $80 $D1 ; "A"
-;            R   E   N   '   T       Y   O   U   ?
-.db $16 $20 $E2 $D5 $DE $B7 $E4 $B0 $E9 $DF $E5 $CF $B0 $B0 $B0 $B0 $B0 $B0 $B0 $B0 $B0 $B0 $B0 $B0
-.db $00 $00
+.INCBIN "src/data/txt/bin/message2.bin"
 
-; 3rd entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F19D to 1F1B7 (27 bytes)
 _DATA_1F19D_:
-.db $8D $20 $B0
-.db $02 $80 $B0 $B0
-.db $8B $60 $B0
-.db $01 $C0 $B0
-;            T   H   A   N   K       Y   O   U   .
-.db $0A $20 $E4 $D8 $D1 $DE $DB $B0 $E9 $DF $E5 $BE
-.db $00 $00
+.INCBIN "src/data/txt/bin/message3.bin"
 
-; 6th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F1B8 to 1F271 (186 bytes)
 _DATA_1F1B8_:
-.db $9D $20 $B0 $8A $80 $B0 $9B $60 $B0 $89 $C0 $B0 $1A $20 $D9 $B7
-.db $DD $B0 $B2 $E0 $D1 $E0 $D5 $E2 $B0 $D8 $D5 $D1 $D4 $BC $B2
-.dsb 9, $B0
-.db $01 $80 $B0 $99 $60 $B0 $01 $80 $E4 $19 $20 $D8 $D5 $B0 $D6 $D9
-.db $E2 $E3 $E4 $B0 $D8 $D5 $DE $D3 $D8 $DD $D1 $DE $B0 $DF $D6 $B0
-.db $E4 $D8 $D5 $B0 $01 $80 $B0 $99 $60 $B0 $01 $80 $DB $19 $20 $D9
-.db $DE $D7 $BE $B0 $D9 $B7 $DC $DC $B0 $DC $D5 $E4 $B0 $E9 $DF $E5
-.db $B0 $E0 $D1 $E3 $E3 $B0 $D2 $E9 $01 $80 $B0 $99 $60 $B0 $01 $80
-.db $D8 $19 $20 $D5 $E2 $D5 $B0 $D9 $D6 $B0 $E9 $DF $E5 $B0 $E7 $D9
-.db $DE $B0 $E4 $D8 $E2 $D5 $D5 $B0 $B0 $B0 $B0 $B0 $01 $80 $B0 $99
-.db $60 $B0 $01 $80 $B2 $19 $20 $DA $D1 $DE $DB $D5 $DE $B2 $B0 $DD
-.db $D1 $E4 $D3 $D8 $D5 $E3 $BE
-.dsb 9, $B0
-.db $00 $00
+.INCBIN "src/data/txt/bin/message4.bin"
 
-; 5th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F272 to 1F32B (186 bytes)
 _DATA_1F272_:
-.db $9D $20 $B0 $8A $80 $B0 $9B $60 $B0 $89 $C0 $B0 $1A $20 $D9 $B7
-.db $DD $B0 $B2 $E3 $D3 $D9 $E3 $E3 $DF $E2 $E3 $B0 $D8 $D5 $D1 $D4
-.db $BC $B2 $B0 $B0 $B0 $B0 $B0 $B0 $01 $80 $B0 $99 $60 $B0 $01 $80
-.db $E4 $19 $20 $D8 $D5 $B0 $E3 $D5 $D3 $DF $DE $D4 $B0 $D8 $D5 $DE
-.db $D3 $D8 $DD $D1 $DE $B0 $DF $D6 $B0 $E4 $D8 $D5 $01 $80 $B0 $99
-.db $60 $B0 $01 $80 $DB $19 $20 $D9 $DE $D7 $BE $B0 $D9 $B7 $DC $DC
-.db $B0 $DC $D5 $E4 $B0 $E9 $DF $E5 $B0 $E0 $D1 $E3 $E3 $B0 $D2 $E9
-.db $01 $80 $B0 $99 $60 $B0 $01 $80 $D8 $19 $20 $D5 $E2 $D5 $B0 $D9
-.db $D6 $B0 $E9 $DF $E5 $B0 $E7 $D9 $DE $B0 $E4 $D8 $E2 $D5 $D5 $B0
-.db $B0 $B0 $B0 $B0 $01 $80 $B0 $99 $60 $B0 $01 $80 $B2 $19 $20 $DA
-.db $D1 $DE $DB $D5 $DE $B2 $B0 $DD $D1 $E4 $D3 $D8 $D5 $E3 $BE
-.dsb 9, $B0
-.db $00 $00
+.INCBIN "src/data/txt/bin/message5.bin"
 
-; 4th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F32C to 1F3E5 (186 bytes)
 _DATA_1F32C_:
-.db $9D $20 $B0 $8A $80 $B0 $9B $60 $B0 $89 $C0 $B0 $1A $20 $D9 $B7
-.db $DD $B0 $B2 $E3 $E4 $DF $DE $D5 $B0 $D8 $D5 $D1 $D4 $BC $B2
-.dsb 9, $B0
-.db $01 $80 $B0 $99 $60 $B0 $01 $80 $E4 $19 $20 $D8 $D5 $B0 $E4 $D8
-.db $D9 $E2 $D4 $B0 $D8 $D5 $DE $D3 $D8 $DD $D1 $DE $B0 $DF $D6 $B0
-.db $E4 $D8 $D5 $B0 $01 $80 $B0 $99 $60 $B0 $01 $80 $DB $19 $20 $D9
-.db $DE $D7 $BE $B0 $D9 $B7 $DC $DC $B0 $DC $D5 $E4 $B0 $E9 $DF $E5
-.db $B0 $E0 $D1 $E3 $E3 $B0 $D2 $E9 $01 $80 $B0 $99 $60 $B0 $01 $80
-.db $D8 $19 $20 $D5 $E2 $D5 $B0 $D9 $D6 $B0 $E9 $DF $E5 $B0 $E7 $D9
-.db $DE $B0 $E4 $D8 $E2 $D5 $D5 $B0 $B0 $B0 $B0 $B0 $01 $80 $B0 $99
-.db $60 $B0 $01 $80 $B2 $19 $20 $DA $D1 $DE $DB $D5 $DE $B2 $B0 $DD
-.db $D1 $E4 $D3 $D8 $D5 $E3 $BE
-.dsb 9, $B0
-.db $00 $00
+.INCBIN "src/data/txt/bin/message6.bin"
 
-; 7th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F3E6 to 1F473 (142 bytes)
 _DATA_1F3E6_:
-.db $9B $20 $B0 $88 $80 $B0 $99 $60 $B0 $87 $C0 $B0 $18 $20 $E9 $DF
-.db $E5 $B0 $DD $E5 $E3 $E4 $B0 $D3 $D8 $DF $DF $E3 $D5 $B0 $D5 $D9
-.db $E4 $D8 $D5 $E2 $B0 $B0 $01 $80 $B0 $97 $60 $B0 $01 $80 $E4 $17
-.db $20 $D8 $D5 $B0 $B2 $E0 $D1 $E0 $D5 $E2 $BC $B2 $B0 $B2 $E3 $D3
-.db $D9 $E3 $E3 $DF $E2 $E3 $BC $B2 $01 $80 $B0 $97 $60 $B0 $01 $80
-.db $DF $17 $20 $E2 $B0 $B2 $E3 $E4 $DF $DE $D5 $B2 $B0 $D2 $D5 $D6
-.db $DF $E2 $D5 $B0 $E4 $D8 $D5 $B0 $B0 $B0 $01 $80 $B0 $97 $60 $B0
-.db $01 $80 $DD $17 $20 $E5 $E3 $D9 $D3 $B0 $E3 $E4 $DF $E0 $E3 $BE
-.dsb 12, $B0
-.db $00 $00
+.INCBIN "src/data/txt/bin/message7.bin"
 
-; 8th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F474 to 1F496 (35 bytes)
 _DATA_1F474_:
-.db $95 $20 $B0 $02 $80 $B0 $B0 $93 $60 $B0 $01 $C0 $B0 $12 $20 $D9
-.db $B0 $E7 $D9 $DE $BE $B0 $E9 $DF $E5 $B0 $D7 $DF $E4 $B0 $D9 $E4
-.db $BE $00 $00
+.INCBIN "src/data/txt/bin/message8.bin"
 
-; 9th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F497 to 1F4B7 (33 bytes)
 _DATA_1F497_:
-.db $93 $20 $B0 $02 $80 $B0 $B0 $91 $60 $B0 $01 $C0 $B0 $10 $20 $D4
-.db $D1 $E2 $DE $B0 $D9 $E4 $BE $B0 $D9 $B0 $DC $DF $E3 $D5 $BE $00
-.db $00
+.INCBIN "src/data/txt/bin/message9.bin"
 
-; 10th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F4B8 to 1F4F9 (66 bytes)
 _DATA_1F4B8_:
-.db $97 $20 $B0 $84 $80 $B0 $95 $60 $B0 $83 $C0 $B0 $14 $20 $D9 $E4
-.db $B7 $E3 $B0 $D1 $B0 $D4 $E2 $D1 $E7 $BE $B0 $B0 $B0 $B0 $B0 $B0
-.db $B0 $B0 $01 $80 $B0 $93 $60 $B0 $01 $80 $E9 $13 $20 $DF $E5 $B0
-.db $E3 $E5 $E2 $D5 $B0 $DC $E5 $D3 $DB $D5 $D4 $B0 $DF $E5 $E4 $BE
-.db $00 $00
+.INCBIN "src/data/txt/bin/message10.bin"
 
-; 11th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F4FA to 1F568 (111 bytes)
 _DATA_1F4FA_:
-.db $9C $20 $B0 $86 $80 $B0 $9A $60 $B0 $85 $C0 $B0 $19 $20 $E7 $D5
-.db $DC $DC $B0 $D9 $E4 $B0 $DC $DF $DF $DB $E3 $B0 $DC $D9 $DB $D5
-.db $B0 $E4 $D8 $D1 $E4 $B7 $E3 $01 $80 $B0 $98 $60 $B0 $01 $80 $E4
-.db $18 $20 $D8 $D5 $B0 $E7 $D1 $E9 $B0 $D9 $E4 $B7 $E3 $B0 $DD $D5
-.db $D1 $DE $E4 $B0 $E4 $DF $B0 $D2 $D5 $BE $01 $80 $B0 $98 $60 $B0
-.db $01 $80 $DF $18 $20 $DB $BE $B0 $E4 $D1 $DB $D5 $B0 $E4 $D8 $D9
-.db $E3 $B1
-.dsb 11, $B0
-.db $00 $00
+.INCBIN "src/data/txt/bin/message11.bin"
 
-; 12th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F569 to 1F663 (251 bytes)
 _DATA_1F569_:
-.db $9C $20 $B0 $8E $80 $B0 $9A $60 $B0 $8D $C0 $B0 $19 $20 $D9 $E4
-.db $B7 $E3 $B0 $DC $E5 $D3 $DB $E9 $B0 $E4 $D8 $D1 $E4 $B0 $E9 $DF
-.db $E5 $B0 $D8 $D1 $E6 $D5 $B0 $01 $80 $B0 $98 $60 $B0 $01 $80 $DD
-.db $18 $20 $D1 $DE $D1 $D7 $D5 $D4 $B0 $E4 $DF $B0 $D3 $DF $DD $D5
-.db $B0 $E4 $D8 $D9 $E3 $B0 $D6 $D1 $E2 $BC $01 $80 $B0 $98 $60 $B0
-.db $01 $80 $D8 $18 $20 $DF $E7 $D5 $E6 $D5 $E2 $BC $B0 $D9 $B7 $DC
-.db $DC $B0 $E0 $E5 $E4 $B0 $D1 $DE $B0 $D5 $DE $D4 $B0 $01 $80 $B0
-.db $98 $60 $B0 $01 $80 $E4 $18 $20 $DF $B0 $E4 $D8 $D1 $E4 $BE $B0
-.db $DC $D5 $E4 $B7 $E3 $B0 $B2 $DA $D1 $DE $DB $D5 $DE $B2 $B0 $B0
-.db $01 $80 $B0 $98 $60 $B0 $01 $80 $D6 $18 $20 $DF $E2 $B0 $E4 $D8
-.db $E2 $D5 $D5 $B0 $DD $D1 $E4 $D3 $D8 $D5 $E3 $B0 $D1 $DE $D4 $B0
-.db $D9 $D6 $B0 $01 $80 $B0 $98 $60 $B0 $01 $80 $E9 $18 $20 $DF $E5
-.db $B0 $DC $DF $E3 $D5 $BC $B0 $D9 $B7 $DC $DC $B0 $DD $D1 $DB $D5
-.db $B0 $E9 $DF $E5 $B0 $B0 $01 $80 $B0 $98 $60 $B0 $01 $80 $E4 $18
-.db $20 $E5 $E2 $DE $B0 $D9 $DE $E4 $DF $B0 $D1 $B0 $E3 $E4 $DF $DE
-.db $D5 $BE $B0 $B0 $B0 $B0 $B0 $B0 $B0 $00 $00
+.INCBIN "src/data/txt/bin/message12.bin"
 
-; 13th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F664 to 1F7DB (376 bytes)
 _DATA_1F664_:
-.db $9E $20 $B0 $94 $80 $B0 $9C $60 $B0 $93 $C0 $B0 $1B $20 $E7 $D5
-.db $DC $D3 $DF $DD $D5 $BC $B0 $D1 $DC $D5 $E8 $BE $B0 $E9 $DF $E5
-.db $B0 $D1 $E2 $D5 $B0 $D1 $B0 $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01
-.db $80 $E0 $1A $20 $E2 $D9 $DE $D3 $D5 $B0 $D6 $E2 $DF $DD $B0 $E4
-.db $D8 $D5 $B0 $D3 $DF $E5 $DE $E4 $E2 $E9 $B0 $DF $D6 $B0 $01 $80
-.db $B0 $9A $60 $B0 $01 $80 $E2 $1A $20 $D1 $D4 $D1 $E8 $D9 $D1 $DE
-.db $BC $B0 $E7 $D8 $DF $B0 $E7 $D1 $E3 $B0 $DB $D9 $D4 $DE $D1 $E0
-.db $E0 $D5 $D4 $01 $80 $B0 $9A $60 $B0 $01 $80 $D2 $1A $20 $E9 $B0
-.db $D5 $E6 $D9 $DC $B0 $DD $D5 $DE $B0 $E7 $D8 $D5 $DE $B0 $E9 $DF
-.db $E5 $B0 $E7 $D5 $E2 $D5 $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80
-.db $D2 $1A $20 $E5 $E4 $B0 $D1 $B0 $E3 $DD $D1 $DC $DC $B0 $D2 $DF
-.db $E9 $BE $E9 $DF $E5 $E2 $B0 $DE $D1 $E4 $D9 $E6 $D5 $01 $80 $B0
-.db $9A $60 $B0 $01 $80 $DC $1A $20 $D1 $DE $D4 $B0 $D9 $E3 $B0 $DE
-.db $DF $E7 $B0 $D2 $D5 $D9 $DE $D7 $B0 $D7 $E2 $DF $E3 $E3 $DC $E9
-.db $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80 $DD $1A $20 $D9 $E3 $D7
-.db $DF $E6 $D5 $E2 $DE $D5 $D4 $B0 $D2 $E9 $B0 $E4 $D8 $D5 $B0 $E4
-.db $E9 $E2 $D1 $DE $E4 $BC $B0 $01 $80 $B0 $9A $60 $B0 $01 $80 $B2
-.db $1A $20 $DA $D1 $DE $DB $D5 $DE $B0 $E4 $D8 $D5 $B0 $D7 $E2 $D5
-.db $D1 $E4 $BE $B2 $B0 $E9 $DF $E5 $E2 $B0 $B0 $B0 $01 $80 $B0 $9A
-.db $60 $B0 $01 $80 $DD $1A $20 $D9 $E3 $E3 $D9 $DF $DE $B0 $D9 $E3
-.db $B0 $E4 $DF $B0 $E3 $D1 $E6 $D5 $B0 $E4 $D8 $D5 $B0 $B0 $B0 $B0
-.db $B0 $01 $80 $B0 $9A $60 $B0 $01 $80 $E0 $1A $20 $DF $E0 $E5 $DC
-.db $D1 $D3 $D5 $B0 $D6 $E2 $DF $DD $B0 $D8 $D9 $DD $BE
-.dsb 9, $B0
-.db $00 $00
+.INCBIN "src/data/txt/bin/message13.bin"
 
-; 14th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F7DC to 1F8E4 (265 bytes)
 _DATA_1F7DC_:
-.db $9E $20 $B0 $8E $80 $B0 $9C $60 $B0 $8D $C0 $B0 $1B $20 $E0 $E2
-.db $D9 $DE $D3 $D5 $B0 $D1 $DC $D5 $E8 $B0 $DF $D6 $B0 $B2 $E2 $D1
-.db $D4 $D1 $E8 $D9 $D1 $DE $BC $B2 $B0 $01 $80 $B0 $9A $60 $B0 $01
-.db $80 $E9 $1A $20 $DF $E5 $B0 $D1 $E2 $D5 $B0 $DC $DF $DF $DB $D9
-.db $DE $D7 $B0 $E6 $D5 $E2 $E9 $B0 $E7 $D5 $DC $DC $B0 $B0 $01 $80
-.db $B0 $9A $60 $B0 $01 $80 $D9 $1A $20 $DE $D4 $D5 $D5 $D4 $B1 $B0
-.db $E7 $D5 $B0 $D8 $D5 $D1 $E2 $B0 $E4 $D8 $D1 $E4 $B0 $E9 $DF $E5
-.db $E2 $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80 $D5 $1A $20 $DC $D4
-.db $D5 $E2 $B0 $D2 $E2 $DF $E4 $D8 $D5 $E2 $B0 $D9 $E3 $B0 $D9 $DD
-.db $E0 $E2 $D9 $E3 $DF $DE $D5 $D4 $01 $80 $B0 $9A $60 $B0 $01 $80
-.db $D9 $1A $20 $DE $B0 $E4 $D8 $D5 $B0 $B2 $E2 $D1 $D4 $D1 $E8 $D9
-.db $D1 $DE $B2 $B0 $D3 $D1 $E3 $E4 $DC $D5 $B0 $B0 $B0 $01 $80 $B0
-.db $9A $60 $B0 $01 $80 $D1 $1A $20 $DE $D4 $B0 $E9 $DF $E5 $B0 $D1
-.db $E2 $D5 $B0 $E4 $D8 $D5 $B0 $DF $DE $DC $E9 $B0 $E0 $D5 $E2 $E3
-.db $DF $DE $01 $80 $B0 $9A $60 $B0 $01 $80 $E7 $1A $20 $D8 $DF $B0
-.db $D3 $D1 $DE $B0 $D3 $DF $DD $D5 $B0 $E4 $DF $B0 $D8 $D9 $E3 $B0
-.db $E2 $D5 $E3 $D3 $E5 $D5 $BE $00 $00
+.INCBIN "src/data/txt/bin/message14.bin"
 
-; 15th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1F8E5 to 1FA5C (376 bytes)
 _DATA_1F8E5_:
-.db $9E $20 $B0 $94 $80 $B0 $9C $60 $B0 $93 $C0 $B0 $1B $20 $E4 $D8
-.db $D1 $DE $DB $B0 $E9 $DF $E5 $BC $B0 $D1 $DC $D5 $E8 $BE $B0 $E4
-.db $D8 $D5 $B0 $DD $DF $DF $DE $BD $B0 $01 $80 $B0 $9A $60 $B0 $01
-.db $80 $DC $1A $20 $D9 $D7 $D8 $E4 $B0 $E3 $E4 $DF $DE $D5 $B0 $D9
-.db $E3 $B0 $D9 $DE $B0 $E4 $D8 $D5 $B0 $B0 $B0 $B0 $B0 $B0 $01 $80
-.db $B0 $9A $60 $B0 $01 $80 $DE $1A $20 $D9 $D2 $D1 $DE $D1 $B0 $DB
-.db $D9 $DE $D7 $D4 $DF $DD $BC $E3 $DF $B0 $E9 $DF $E5 $B0 $DD $E5
-.db $E3 $E4 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80 $E2 $1A $20 $D5 $D1
-.db $D3 $D8 $B0 $E4 $D8 $D5 $E2 $D5 $B0 $D2 $D5 $D6 $DF $E2 $D5 $B0
-.db $DA $D1 $DE $DB $D5 $DE $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80
-.db $E4 $1A $20 $D8 $D5 $B0 $D7 $E2 $D5 $D1 $E4 $B0 $D4 $DF $D5 $E3
-.db $BE $B0 $D1 $DE $B0 $D1 $E5 $E4 $DF $BD $B0 $B0 $B0 $01 $80 $B0
-.db $9A $60 $B0 $01 $80 $D7 $1A $20 $E2 $D1 $E0 $D8 $D5 $D4 $B0 $DC
-.db $D5 $E4 $E4 $D5 $E2 $B0 $D1 $D4 $D4 $E2 $D5 $E3 $E3 $D5 $D4 $B0
-.db $E4 $DF $01 $80 $B0 $9A $60 $B0 $01 $80 $E4 $1A $20 $D8 $D5 $B0
-.db $DE $D9 $D2 $D1 $DE $D1 $B0 $DB $D9 $DE $D7 $D4 $DF $DD $B0 $D9
-.db $E3 $B0 $B0 $B0 $B0 $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80 $D8
-.db $1A $20 $D9 $D4 $D4 $D5 $DE $B0 $E3 $DF $DD $D5 $E7 $D8 $D5 $E2
-.db $D5 $B0 $D9 $DE $B0 $E4 $D8 $D9 $E3 $B0 $B0 $B0 $01 $80 $B0 $9A
-.db $60 $B0 $01 $80 $D3 $1A $20 $D1 $E3 $E4 $DC $D5 $BE $B0 $D1 $D6
-.db $E4 $D5 $E2 $B0 $D6 $D9 $DE $D4 $D9 $DE $D7 $B0 $D9 $E4 $BC $B0
-.db $B0 $01 $80 $B0 $9A $60 $B0 $01 $80 $E4 $1A $20 $D1 $DB $D5 $B0
-.db $D9 $E4 $B0 $E7 $D9 $E4 $D8 $B0 $E9 $DF $E5 $BE
-.dsb 10, $B0
-.db $00 $00
+.INCBIN "src/data/txt/bin/message15.bin"
 
-; 16th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1FA5D to 1FB40 (228 bytes)
 _DATA_1FA5D_:
-.db $9E $20 $B0 $8C $80 $B0 $9C $60 $B0 $8B $C0 $B0 $1B $20 $DF $D8
-.db $BC $D1 $DC $D5 $E8 $BC $D9 $B7 $DD $B0 $E4 $D5 $E2 $E2 $D9 $D2
-.db $DC $E9 $B0 $E3 $DF $E2 $E2 $E9 $BC $01 $80 $B0 $9A $60 $B0 $01
-.db $80 $D2 $1A $20 $E5 $E4 $B0 $D9 $B0 $E7 $D1 $E3 $B0 $DA $E5 $E3
-.db $E4 $B0 $E2 $DF $D2 $D2 $D5 $D4 $B0 $DF $D6 $B0 $B0 $B0 $01 $80
-.db $B0 $9A $60 $B0 $01 $80 $E4 $1A $20 $D8 $D5 $B0 $DD $DF $DF $DE
-.db $DC $D9 $D7 $D8 $E4 $B0 $E3 $E4 $DF $DE $D5 $BE $B0 $E4 $D8 $D5
-.db $B0 $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80 $B2 $1A $20 $D3 $E2
-.db $DF $E7 $DE $B2 $B0 $D9 $E3 $B0 $D9 $DE $B0 $DC $D1 $DB $D5 $B0
-.db $D4 $D5 $D5 $E0 $BE $B0 $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80
-.db $E9 $1A $20 $DF $E5 $B7 $D4 $B0 $D2 $D5 $E4 $E4 $D5 $E2 $B0 $E4
-.db $D1 $DB $D5 $B0 $E4 $D8 $D9 $E3 $B0 $B0 $B0 $B0 $B0 $01 $80 $B0
-.db $9A $60 $B0 $01 $80 $E3 $1A $20 $E4 $DF $DE $D5 $B0 $E3 $DC $D1
-.db $D2 $B0 $E7 $D9 $E4 $D8 $B0 $E9 $DF $E5 $BE $B0 $B0 $B0 $B0 $B0
-.db $B0 $B0 $00 $00
+.INCBIN "src/data/txt/bin/message16.bin"
 
-; 17th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1FB41 to 1FB88 (72 bytes)
 _DATA_1FB41_:
-.db $9A $20 $B0 $84 $80 $B0 $98 $60 $B0 $83 $C0 $B0 $17 $20 $D9 $B0
-.db $D8 $D1 $E6 $D5 $B0 $DE $DF $B0 $D2 $E5 $E3 $D9 $DE $D5 $E3 $E3
-.db $B0 $E7 $D9 $E4 $D8 $01 $80 $B0 $96 $60 $B0 $01 $80 $E9 $16 $20
-.db $DF $E5 $BC $B0 $E3 $DF $B0 $E0 $DC $D5 $D1 $E3 $D5 $B0 $DC $D5
-.db $D1 $E6 $D5 $BE $B0 $B0 $00 $00
+.INCBIN "src/data/txt/bin/message17.bin"
 
-; 18th entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1FB89 to 1FC47 (191 bytes)
 _DATA_1FB89_:
-.db $9E $20 $B0 $8A $80 $B0 $9C $60 $B0 $89 $C0 $B0 $1B $20 $E4 $D8
-.db $D1 $DE $DB $B0 $E9 $DF $E5 $BC $D1 $DC $D5 $E8 $BE $E9 $DF $E5
-.db $E2 $B0 $DD $DF $E4 $D8 $D5 $E2 $B0 $01 $80 $B0 $9A $60 $B0 $01
-.db $80 $D9 $1A $20 $E3 $B0 $E7 $D5 $DC $DC $B0 $D1 $DE $D4 $B0 $D9
-.db $DE $B0 $D7 $DF $DF $D4 $B0 $D8 $D1 $DE $D4 $E3 $BE $B0 $01 $80
-.db $B0 $9A $60 $B0 $01 $80 $E0 $1A $20 $DC $D5 $D1 $E3 $D5 $B0 $E3
-.db $D1 $E6 $D5 $B0 $E4 $D8 $D5 $B0 $E2 $D1 $D4 $D1 $E8 $D9 $D1 $DE
-.db $B0 $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80 $E0 $1A $20 $D5 $DF
-.db $E0 $DC $D5 $B0 $E7 $D9 $E4 $D8 $B0 $E9 $DF $E5 $E2 $B0 $DD $DF
-.db $E3 $E4 $B0 $B0 $B0 $B0 $B0 $B0 $01 $80 $B0 $9A $60 $B0 $01 $80
-.db $E0 $1A $20 $DF $E7 $D5 $E2 $D6 $E5 $DC $B0 $E7 $D5 $D1 $E0 $DF
-.db $DE $BC $E4 $D8 $D5 $B2 $D3 $E2 $DF $E7 $DE $BE $B2 $00 $00
+.INCBIN "src/data/txt/bin/message18.bin"
 
-; 21st entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1FC48 to 1FC83 (60 bytes)
 _DATA_1FC48_:
-.db $94 $20 $B0 $84 $80 $B0 $92 $60 $B0 $83 $C0 $B0 $11 $20 $E9 $DF
-.db $E5 $B0 $D2 $D5 $E4 $E4 $D5 $E2 $B0 $D1 $D3 $D3 $D5 $E0 $E4 $01
-.db $80 $B0 $90 $60 $B0 $01 $80 $E4 $10 $20 $D8 $D5 $B0 $D9 $DE $D5
-.db $E6 $D9 $E4 $D1 $D2 $DC $D5 $B1 $B0 $B0 $00 $00
+.INCBIN "src/data/txt/bin/message19.bin"
 
-; 22nd entry of Pointer Table from 7F50 (indexed by v_messageToShowInTheTextBoxIndex)
-; Data from 1FC84 to 1FC9D (26 bytes)
 _DATA_1FC84_:
-.db $8C $20 $B0 $02 $80 $B0 $B0 $8A $60 $B0 $01 $C0 $B0 $09 $20 $E3
-.db $DF $DC $D4 $B0 $DF $E5 $E4 $BE $00 $00
+.INCBIN "src/data/txt/bin/message20.bin"
