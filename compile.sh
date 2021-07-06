86b9847d32b5f24035c797b8b79303c14b0f0742
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2021-07-06 18:56:41 -0300

    Update compile script

--- compile.sh
+++ compile.sh
@@ -3,24 +3,15 @@ mkdir -p tmp build
 rm -f tmp/* 
 
 echo Compiling
-wla-z80 -i -I src -D _REV0 -o tmp/baserom_rev0.o src/asm/baserom.asm 
-wla-z80 -i -I src -D _REV1 -o tmp/baserom_rev1.o src/asm/baserom.asm
-
-echo Creating link files
-# Make link file
-echo [objects] > tmp/linkfile_rev0
-echo baserom_rev0.o >> tmp/linkfile_rev0
-
-echo [objects] > tmp/linkfile_rev1
-echo baserom_rev1.o >> tmp/linkfile_rev1
+wla-z80 -I src -D _REV0 -o tmp/baserom_rev0.o src/asm/baserom.asm 
+wla-z80 -I src -D _REV1 -o tmp/baserom_rev1.o src/asm/baserom.asm
 
 rm -f build/*
 
 echo Linking
-cd tmp
-wlalink -d -S -i -b linkfile_rev0 ../build/alex-kidd-in-miracle-world-usa-europe.sms
-wlalink -d -S -i -b linkfile_rev1 ../build/alex-kidd-in-miracle-world-usa-europe-rev-1.sms
-cd ..
+wlalink -d -S -b linkfile_rev0 build/alex-kidd-in-miracle-world-usa-europe.sms
+wlalink -d -S -b linkfile_rev1 build/alex-kidd-in-miracle-world-usa-europe-rev-1.sms
+
 
 if sha1sum --status -c <<<"8cecf8ed0f765163b2657be1b0a3ce2a9cb767f4 *build/alex-kidd-in-miracle-world-usa-europe.sms"; then
     echo "REV0 Ok!"
