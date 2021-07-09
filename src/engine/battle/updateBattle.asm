0189e905803391a43340ad7ffc5fafd775e8bc18
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2021-07-09 02:56:15 -0300

    Move Janken updaters back

--- src/engine/battle/updateBattle.asm
+++ src/engine/battle/updateBattle.asm
@@ -1,26 +1,3 @@
-jankenUpdaters:
-.dw updateBattleInit
-.dw updateBattleMakeAlexGetIntoPosition
-.dw updateBattleLoadOpponentTilesAndShowTextbox1
-.dw updateBattleShowTextbox2
-.dw updateBattleStartRound
-.dw updateBattleDance
-.dw updateBattleThrow
-.dw updateBattleHandleThrows
-.dw updateBattleShowBattleLostTextbox
-.dw updateBattleTurnAlexIntoStatue
-.dw updateBattleRespawOpponent
-.dw _LABEL_73AE_
-.dw updateBattleStartFight
-.dw updateBattlePatchNametable
-.dw _LABEL_73D8_
-.dw _LABEL_7447_
-.dw updateBattleMakeAlexGetIntoPosition
-.dw _LABEL_7453_
-.dw _LABEL_746F_
-.dw updateBattlePatchNametable
-.dw updateBattleNop
-
 ; - Increment state (0x0 to 0x1: updateBattleMakeAlexGetIntoPosition)
 ; - Maybe set opponent sprite descriptor pointer
 ; - Copy opponent settings
--- src/entities/updateJanken.asm
+++ src/entities/updateJanken.asm
@@ -4,3 +4,25 @@ updateJanken:
     rst jumpToAthPointer
     ret
 
+jankenUpdaters:
+.dw updateBattleInit
+.dw updateBattleMakeAlexGetIntoPosition
+.dw updateBattleLoadOpponentTilesAndShowTextbox1
+.dw updateBattleShowTextbox2
+.dw updateBattleStartRound
+.dw updateBattleDance
+.dw updateBattleThrow
+.dw updateBattleHandleThrows
+.dw updateBattleShowBattleLostTextbox
+.dw updateBattleTurnAlexIntoStatue
+.dw updateBattleRespawOpponent
+.dw _LABEL_73AE_
+.dw updateBattleStartFight
+.dw updateBattlePatchNametable
+.dw _LABEL_73D8_
+.dw _LABEL_7447_
+.dw updateBattleMakeAlexGetIntoPosition
+.dw _LABEL_7453_
+.dw _LABEL_746F_
+.dw updateBattlePatchNametable
+.dw updateBattleNop
