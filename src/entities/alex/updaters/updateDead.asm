a8f7d891aebd86ddd00a78e7e919db3b418f18bd
Lucas Azevedo <lhs_azevedo@hotmail.com>
2021-08-17 02:07:44 -0300

    Analyze alex dead updater

--- src/entities/alex/updaters/updateDead.asm
+++ src/entities/alex/updaters/updateDead.asm
@@ -1,16 +1,20 @@
-; 16th entry of Jump Table from 2982 (indexed by v_alex.state)
 updateAlexDead:
     ld hl, alexDeadAnimationDescriptor
     call loadAlexAnimationDescriptor
+
     ld a, (v_alex.yPos.high)
     cp $A8
     ret nc
+
     cp $A3
     ret c
+
     ld a, (v_alex.isOffScreenFlags.high)
     inc a
     ret nz
+
     call destroyCurrentEntity
-    ld a, $06
+
+    ld a, STATE_LIFE_LOST
     ld (v_gameState), a
     ret
