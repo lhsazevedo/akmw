ddeb6df62412dfe1d2bbed52d75f4a43c598bb31
Lucas Azevedo <lhs_azevedo@hotmail.com>
2021-10-02 00:07:31 -0300

    Exit with error on non-matching builds

--- compile.sh
+++ compile.sh
@@ -13,14 +13,15 @@ wlalink -d -S -b linkfile_rev0 build/alex-kidd-in-miracle-world-usa-europe.sms
 wlalink -d -S -b linkfile_rev1 build/alex-kidd-in-miracle-world-usa-europe-rev-1.sms
 
 
-if sha1sum --status -c <<<"8cecf8ed0f765163b2657be1b0a3ce2a9cb767f4 *build/alex-kidd-in-miracle-world-usa-europe.sms"; then
-    echo "REV0 Ok!"
-else
+if ! sha1sum --status -c <<<"8cecf8ed0f765163b2657be1b0a3ce2a9cb767f4 *build/alex-kidd-in-miracle-world-usa-europe.sms"; then
     echo "REV0 Diff :/"
+    exit 1
+fi
+echo "REV0 Ok!"
+
+if ! sha1sum --status -c <<<"6d052e0cca3f2712434efd856f733c03011be41c *build/alex-kidd-in-miracle-world-usa-europe-rev-1.sms"; then
+    echo "REV1 Diff :/"
+    exit 1
 fi
 
-if sha1sum --status -c <<<"6d052e0cca3f2712434efd856f733c03011be41c *build/alex-kidd-in-miracle-world-usa-europe-rev-1.sms"; then
-    echo "REV1 Ok!"
-else
-    echo "REV1 Diff :/"
-fi
+echo "REV1 Ok!"
